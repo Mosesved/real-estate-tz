79ab49b3739b926c3b992129db6d8812650b4a17
Mosesved <safarmagogwa@gmail.com>
2026-01-29 23:34:29 +0300

    Update Index.js

--- backend/backend/app/Index.js
+++ backend/backend/app/Index.js
@@ -1,1 +1,28 @@
+
+const express = require("express");
+const app = express();
+
+// ==================
+// MIDDLEWARE
+// ==================
+app.use(express.json());
+
+// ==================
+// ROUTES
+// ==================
 const listingRoutes = require("../listings/listing.routes");
+
+// LISTING API
+app.use("/api/listings", listingRoutes);
+
+// ==================
+// TEST ROUTE (OPTIONAL)
+// ==================
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// ==================
+// EXPORT APP
+// ==================
+module.exports = app;
