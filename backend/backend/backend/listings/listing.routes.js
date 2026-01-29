108c13401129a4b9044d7aa4154b93d1d5848b32
Mosesved <safarmagogwa@gmail.com>
2026-01-29 23:27:50 +0300

    Update listing.routes.js

--- backend/backend/backend/listings/listing.routes.js
+++ backend/backend/backend/listings/listing.routes.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+  createListing,
+  getAllListings,
+  getSingleListing,
+  updateListing,
+  deleteListing,
+} = require("./listing.controller");
+
+// CREATE
+router.post("/", createListing);
+
+// READ ALL
+router.get("/", getAllListings);
+
+// READ ONE
+router.get("/:id", getSingleListing);
+
+// UPDATE
+router.put("/:id", updateListing);
+
+// DELETE
+router.delete("/:id", deleteListing);
+
+module.exports = router;
