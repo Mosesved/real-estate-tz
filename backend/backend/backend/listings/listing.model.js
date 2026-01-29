8b868b062742650dc79214913f940b7e1b2db71e
Mosesved <safarmagogwa@gmail.com>
2026-01-29 23:25:22 +0300

    Update listing.model.js

--- backend/backend/backend/listings/listing.model.js
+++ backend/backend/backend/listings/listing.model.js
@@ -1,1 +1,56 @@
+const mongoose = require("mongoose");
 
+const listingSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: ["house", "plot", "apartment", "room"],
+      required: true,
+    },
+
+    agentName: {
+      type: String,
+      required: true,
+    },
+
+    agentPhone: {
+      type: String,
+      required: true,
+    },
+
+    images: [
+      {
+        type: String,
+      },
+    ],
+
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Listing", listingSchema);
