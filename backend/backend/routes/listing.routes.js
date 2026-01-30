7a265f0549e73fdb9f190a497adb3b617910dd5a
Mosesved <safarmagogwa@gmail.com>
2026-01-30 15:14:03 +0300

    Update listing.routes.js

--- backend/backend/routes/listing.routes.js
+++ backend/backend/routes/listing.routes.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { getAllListings } from "../controllers/listing.controller.js";
 
+const router = express.Router();
+
+// GET all listings
+router.get("/", getAllListings);
+
+export default router;
